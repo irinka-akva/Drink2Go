f32fecc38a5a34b0ca057712b09bb54e1f1b11a6
Irina <irinka-akva@ya.ru>
2023-02-11 23:42:56 +0300

    Добавляет частичную адаптацию сайта под десктопную и планшетную версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,19 @@
-/* в этот файл добавляет скрипты*/
+/* navigation*/
+
+let nav = document.querySelector('.navigation');
+let navToggle = document.querySelector('.navigation__toggle');
+
+nav.classList.remove('navigation--no-js');
+nav.classList.add('navigation--close');
+
+navToggle.addEventListener('click', function () {
+  if (nav.classList.contains('navigation--close')) {
+    nav.classList.remove('navigation--close');
+    nav.classList.add('navigation--open');
+  } else {
+    nav.classList.add('navigation--close');
+    nav.classList.remove('navigation--open');
+  }
+});
+
+
