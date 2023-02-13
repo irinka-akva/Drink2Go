232ea1418ef606f13a0480c35e967452ebd7dc81
Irina <irinka-akva@ya.ru>
2023-02-14 02:51:21 +0300

    Исправляет ошибки после тестирования и добавляет интерактивный блок с картой

--- source/js/script.js
+++ source/js/script.js
@@ -16,4 +16,37 @@ navToggle.addEventListener('click', function () {
   }
 });
 
+/* map*/
 
+const map = L.map('map')
+  .setView({
+    lat: 59.968137,
+    lng: 30.316272,
+  }, 100);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/map-pin.svg',
+  iconSize: [38, 50],
+  iconAnchor: [19, 50],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: 59.968137,
+    lng: 30.316272,
+  },
+  {
+    icon:mainPinIcon,
+  },
+);
+
+mainPinMarker.addTo(map);
+
+/* slider*/
